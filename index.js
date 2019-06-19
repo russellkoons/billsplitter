0f23db57b9ce20390e70900ad8d2209e0ed6417b
russellkoons <rkoons@gmail.com>
2019-06-19 14:54:00 -0400

    JS functions working

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 function calculate(e) {
   e.preventDefault();
-  console.log('Hello world!');
+
+  const bill = document.getElementById('bill').value;
+
+  if (isNaN(bill)) {
+    console.log('Not a number');
+    return;
+  }
+
+  const party = document.getElementById('party').value;
+  const tip = document.getElementById('tip').value / 100;
+
+  console.log(bill, party, tip);
+
+  const splitBill = Math.round(bill / party * 100) / 100;
+  const splitTip = Math.round(bill * tip / party * 100) / 100;
+
+  console.log(splitBill, splitTip);
 }
