ce111e05a167123caa43159830636090a978743d
russellkoons <rkoons@gmail.com>
2019-06-19 15:20:09 -0400

    finished js

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 function calculate(e) {
   e.preventDefault();
 
-  const bill = document.getElementById('bill').value;
+  const bill = $('#bill').val();
 
   if (isNaN(bill)) {
-    console.log('Not a number');
+    $('#result').empty().append(
+      '<p>Please enter a valid number</p>'
+    );
     return;
   }
 
-  const party = document.getElementById('party').value;
-  const tip = document.getElementById('tip').value / 100;
-
-  console.log(bill, party, tip);
+  const party = $('#party').val();
+  const tip = $('#tip').val() / 100;
 
   const splitBill = Math.round(bill / party * 100) / 100;
   const splitTip = Math.round(bill * tip / party * 100) / 100;
 
-  console.log(splitBill, splitTip);
+  if (splitTip === 0) {
+    $('#result').empty().append(
+      `<p>Your split bill for ${party} people is...</p>
+      <p>$${splitBill.toFixed(2)}</p>`
+    );
+  } else {
+    $('#result').empty().append(
+      `<p>Your split bill for ${party} people is...</p>
+      <p id="splitbill">$${splitBill.toFixed(2)}</p>
+      <p>Your tip is...</p>
+      <p id="splittip">$${splitTip.toFixed(2)}</p>`
+    );
+  }
 }
